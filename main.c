c4b20dca0f77343feecffa165a5ab0e4bf593de3
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-17 18:11:46 +0100

    overall updates

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #define SIZE 5
 #define TILES_NEEDED_TO_WIN 3
 int game_grid[SIZE][SIZE];
-//int tilesNeededToWin = 3;
 int const players[2] = {'X', 'O'};
 int current_player = 0;
 
+// funkce pro vyplneni gridu pomlckami
 void setup(){
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
@@ -15,6 +15,17 @@ void setup(){
     }
 }
 
+// kontrola jestli je nekdo vitez
+int isWinner(int numInLine){
+    printf("numInLine: %d\n", numInLine);
+    if(numInLine >= TILES_NEEDED_TO_WIN){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+// funkce pro ziskavani informaci od uzivatele
 int get_info(char info[]){
     int answer;
     int pocetZnaku;
@@ -29,87 +40,109 @@ int get_info(char info[]){
             printf("You didn't enter a number!\n");
         }else if(answer < 0 || answer >= SIZE){
             printf("You entered a number out of range!\n");
-        }else if(game_grid[answer][answer] != '-'){
-            printf("This field is already taken!\n");
         }
-    }while(pocetZnaku > 0 || answer < 0 || answer >= SIZE || game_grid[answer][answer] != '-');
+    }while(pocetZnaku > 0 || answer < 0 || answer >= SIZE);
     return answer;
 }
 
+// funkce pro vypis cary
 void line(){
-    printf("-------------------------\n");
+    printf("//////////////////////////////////////////////\n");
 }
 
+// funkce pro vypis vnitrni vodorovne cary gridu
+void gridLine(){
+    printf("|");
+    for(int i = 0; i < SIZE; i++){
+        printf("----");
+    }
+    printf("|\n");
+}
+
+// funkce pro vypis gridu
 void printGrid(){
     line();
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
+            printf("| ");
             printf("%c ", game_grid[i][j]);
         }
-        printf("\n");
+        printf(" |\n");
+        if (i < SIZE - 1){ // tohle zabranuje vytvareni cary pod poslednim radkem
+            gridLine();
+        }
     }
     line();
-    printf("Current player: %c\n", players[current_player]);
 }
 
+// funkce pro kontrolu jestli je nekdo vitez
 int checkWinner(int row, int column){
     int numInLine = 1;
     int checked = 1;
-    //printf("grid: %c\n", game_grid[row][column]);
-    /*int testGrid[SIZE][SIZE];
-    for (int i = 0; i < SIZE; i++){
-        for (int j = 0; j < SIZE; j++){
-            testGrid[i][j] = 0;
-        }
-    } */
     // kontrola radku doleva a doprava
-    while(numInLine < TILES_NEEDED_TO_WIN && checked < TILES_NEEDED_TO_WIN){
+    while(numInLine < TILES_NEEDED_TO_WIN && checked < TILES_NEEDED_TO_WIN && column - checked >= 0){
         if(game_grid[row][column] == game_grid[row][column - checked]){
             numInLine++;
-            //printf("added\n");
-            //testGrid[row][column - checked] = 1;
         } else {
-            numInLine = 0;
             break;
         }
         checked++;
     }
     checked = 1;
-    while(numInLine < TILES_NEEDED_TO_WIN && checked < TILES_NEEDED_TO_WIN){
+    while(numInLine < TILES_NEEDED_TO_WIN && checked < TILES_NEEDED_TO_WIN && column + checked < SIZE){
         if(game_grid[row][column] == game_grid[row][column + checked]){
             numInLine++;
-            //printf("added here\n");
-            //testGrid[row][column + checked] = 1;
         } else {
-            numInLine = 0;
             break;
         }
     }
-    checked = 0;
-    //printf("numInLine: %d\n", numInLine);
-    /*for (int i = 0; i < SIZE; i++){
-        for (int j = 0; j < SIZE; j++){
-            printf("%d ", testGrid[i][j]);
-        }
-        printf("\n");
-    } */
-    // kontrola jestli je nekdo vitez
-    if(numInLine >= TILES_NEEDED_TO_WIN){
+    checked = 1;
+    if (isWinner(numInLine) == 1){
         return 1;
     }else{
-        return 0;
+        numInLine = 1;
+    }
+    // kontrola sloupce nahoru a dolu
+    while(numInLine < TILES_NEEDED_TO_WIN && checked < TILES_NEEDED_TO_WIN && row - checked >= 0){
+        if(game_grid[row][column] == game_grid[row - checked][column]){
+            numInLine++;
+            printf("added from first if\n");
+        } else {
+            break;
+        }
+    }
+    checked = 1;
+    while(numInLine < TILES_NEEDED_TO_WIN && checked < TILES_NEEDED_TO_WIN && row + checked < SIZE){
+        if(game_grid[row][column] == game_grid[row + checked][column]){
+            numInLine++;
+            printf("added from second if\n");
+        } else {
+            break;
+        }
+    }
+    if (isWinner(numInLine) == 1){
+        return 1;
+    }else{
+        numInLine = 1;
     }
 }
 
 int main(){
     setup();
     int running = 1;
-    int row, column;
+    int row, column = -1;
     printf("Current player: %c\n", players[current_player]);
     while(running){
         // ziskani souradnic
-        row = get_info("row");
-        column = get_info("column");
+        do{
+            row = get_info("row");
+            column = get_info("column");
+            if (game_grid[row][column] != '-' && row >= 0 && column >= 0){
+                printf("This field is already taken!\n");
+            }
+            printf("row: %d, column: %d\n", row, column);
+        }while (game_grid[row][column] != '-');
+        
         game_grid[row][column] = players[current_player];
         // vypis gridu
         printGrid();
@@ -117,11 +150,15 @@ int main(){
         if (checkWinner(row, column)==1){
             printf("Player %c wins!\n", players[current_player]);
             running = 0;
+            break;
         }
         if (current_player == 0){
             current_player = 1;
         } else {
             current_player = 0;
         }
+        row = -1;
+        column = -1;
+        printf("Current player: %c\n", players[current_player]);
     }
 }
